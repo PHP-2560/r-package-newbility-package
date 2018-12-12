0bca4dc8d176f82cb28c119cb522c539b4c4b836
YanZhaoBrown <43217741+YanZhaoBrown@users.noreply.github.com>
2018-12-12 14:40:05 -0500

    stringr::

--- game24/R/game24_check_probability.R
+++ game24/R/game24_check_probability.R
@@ -109,7 +109,7 @@ game24_table=function(A,b=24){
   result2=result2[-rep]
 
   for(f in 1:length(operation_1)){
-    method=c(method,str_c(operation_1[f]," = ", result1[f], " then ",
+    method=c(method,stringr::str_c(operation_1[f]," = ", result1[f], " then ",
                           operation_2[f]," = ", result2[f], " then ",
                           operation_3[f],  " = ", b))
   }
